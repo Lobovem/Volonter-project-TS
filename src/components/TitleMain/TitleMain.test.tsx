5d9f00e7be7f63df4994161bcf30398d156c216c
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-29 19:42:38 +0200

    Done title

--- src/components/TitleMain/TitleMain.test.tsx
+++ src/components/TitleMain/TitleMain.test.tsx
@@ -4,22 +4,22 @@ import '@testing-library/jest-dom';
 import { TitleMain } from './TitleMain';
 
 describe('TitleMain', () => {
+  const testTitle = 'Title';
   test('renders TitleMain component', () => {
     // eslint-disable-next-line testing-library/render-result-naming-convention
-    const component = render(<TitleMain />);
+    const component = render(<TitleMain title = {testTitle}/>);
     expect(component).toMatchSnapshot();
   });
 
   test('search tag h2 title', () => {
-    render(<TitleMain />);
+    render(<TitleMain title = {testTitle}/>);
     const h3Element = screen.getByRole('heading', { level: 2 });
     expect(h3Element).toBeInTheDocument();
   });
 
   test('search text tag h2 title', () => {
-    const title = 'Title';
 
-    render(<TitleMain title={title} />);
+    render(<TitleMain title={testTitle} />);
     const titleElem = screen.getByRole('heading', { name: /title/i });
     expect(titleElem).toBeInTheDocument();
   });
--- src/components/TitleMain/TitleMain.tsx
+++ src/components/TitleMain/TitleMain.tsx
@@ -1,6 +1,6 @@
 import s from './TitleMain.module.scss';
 
-export const TitleMain = (props) => {
+export const TitleMain = (props:{title:string}) => {
   const { title } = props;
   return <h2 className={s.titleMain}>{title}</h2>;
 };
