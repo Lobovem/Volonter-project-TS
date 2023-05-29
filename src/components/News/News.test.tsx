cabeeef5c900c6447d705fab5c5ef2364c5b7f11
Lobov Evgeniy <lobov.em@gmail.com>
2023-05-29 15:12:39 +0200

    Done news

--- src/components/News/News.test.tsx
+++ src/components/News/News.test.tsx
@@ -2,15 +2,15 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../redux/config/store';
 import { News } from './News';
-import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('News', () => {
   test('renders News component', () => {
     const { container } = render(
       <Provider store={store}>
-        <Router>
+        <MemoryRouter>
           <News />
-        </Router>
+        </MemoryRouter>
       </Provider>
     );
     expect(container).toMatchSnapshot();
